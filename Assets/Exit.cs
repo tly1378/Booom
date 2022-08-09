83955e0c9e618e45aba899dbdc92c3d80091bf33
“Nasijkl” <745223564@qq.com>
2022-08-09 20:18:17 +0800

    temp

--- Assets/Exit.cs
+++ Assets/Exit.cs
@@ -6,16 +6,16 @@ public class Exit : MonoBehaviour
 {
     public GameObject exit;
     public Animator exitt;
-    private int one = 0 ;
+    private int one = 0,two =0 ;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)&& one ==0)
+        if (Input.GetKeyDown(KeyCode.Escape)&& one ==0 &&two ==0)
         {
             exit.SetActive(true);
             one = 1;
         }
-        if (Input.GetKeyDown(KeyCode.Escape) && one == 1)
+        if (Input.GetKeyDown(KeyCode.Escape) && one == 1 )
         {
             exitt.SetBool("end",true);
             one = 0;
