557aab27e775dccaab1d9a775b9514df4390e2a3
“Nasijkl” <745223564@qq.com>
2022-08-09 20:06:05 +0800

    demo11

---
+++ Assets/Exit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public GameObject exit;
+    public Animator exitt;
+    private int one = 0 ;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)&& one ==0)
+        {
+            exit.SetActive(true);
+            one = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && one == 1)
+        {
+            exitt.SetBool("end",true);
+            one = 0;
+        }
+    }
+   
+}
---
+++ Assets/Exit1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit1 : MonoBehaviour
+{
+    public GameObject exit;
+    public void sfalse()
+    {
+        exit.SetActive(false);
+    }
+}
